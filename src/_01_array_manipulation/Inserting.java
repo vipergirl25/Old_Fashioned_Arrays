8d3089cb4a747e41f666f82e7618164da3da1c01
league <league@wts21.attlocal.net>
2018-06-17 11:24:51 -0700

    party

--- src/_01_array_manipulation/Inserting.java
+++ src/_01_array_manipulation/Inserting.java
@@ -3,5 +3,16 @@ package _01_array_manipulation;
 
 
 public class Inserting {
+
+	public static int[] insertAt(int[] testArray, int i, int j) {
+		//make a new array
+		//insert the element at the thingy
+		//you are done, you have passed the test
+		//i = the thing you are inserting
+		//j = the place you are putting i
+		int[] newArray = new int[testArray.length + 1];
+		//help me i have no clue how to do this
+		return newArray;
+	}
 	
 }
--- src/_01_array_manipulation/Sorting.java
+++ src/_01_array_manipulation/Sorting.java
@@ -2,4 +2,41 @@ package _01_array_manipulation;
 
 public class Sorting {
 
+	public static void sort(String[] testArray) {
+		String word1 = "";
+		String word2 = "";
+		int compare = 0;
+		boolean swap = false;
+		boolean test = true;
+		int swapcounter = 0;
+		while(test == true) {
+			swapcounter=0;
+			System.out.println(swapcounter);
+			for (int i = 0; i < testArray.length-1; i++) {
+				swap = false;
+				word1 = testArray[i];
+				word2 = testArray[i+1];
+				compare = word1.compareTo(word2);
+				System.out.println(compare);
+				if(compare <= -1) {
+					
+				}else if(compare == 0) {
+					
+				}else {
+					testArray[i]=word2;
+					testArray[i+1]=word1;
+					swapcounter++;
+					System.out.println(swapcounter);
+				}
+			}
+			if(swapcounter==0) {
+				test=false;
+			}else {
+				test=true;
+			}
+		}
+	
+		
+	}
+
 }
